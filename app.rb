63e7a477d0fa60bf00188026ab69fe39265c65ba
Cristiano Betta <cbetta@gmail.com>
2016-08-15 14:52:45 +0100

    Add comments in app.rb

--- app.rb
+++ app.rb
@@ -1,37 +1,59 @@
+# web server and flash messages
 require 'sinatra'
 require 'rack-flash'
-require 'nexmo'
-require 'dotenv'
-
-Dotenv.load
 use Rack::Flash
 
-enable :sessions
-set :session_secret, '1a90214966e7d7ad57aef6874871ba78'
-set :erb, layout: :layout
+# load environment variables from .env file
+require 'dotenv'
+Dotenv.load
 
+# nexmo library
+require 'nexmo'
 nexmo = Nexmo::Client.new
 
-before do
-  @current_user = session[:current_user]
-end
+# enable sessions and set the session secret
+enable :sessions
+set :session_secret, '123456'
 
+# specify a default layout
+set :erb, layout: :layout
+
+# Index
+# - tries to show the current user's
+#   phone number if it is present
+#
 get '/' do
+  @current_user = session[:current_user]
   erb :index
 end
 
+# Login
+# - presents the user with a form to
+#   fill in their phone number
+#
 get '/login' do
   erb :login
 end
 
+# Start Verification
+# - starts the verification process, sending
+#   a code to the user's phone number and
+#   then redirecting them to verify the code
+#
 post '/start_verification' do
+  # start the verification request
   response = nexmo.start_verification(
     number: params['number'],
     brand: 'MyApp'
   )
 
+  # any status that's not '0' is an error
   if response['status'] == '0'
+    # save the number so we can show it
+    # once the user is logged in
     session[:number] = params['number']
+    # save the verification ID so we can verify the user's
+    # code against it in the next step
     session[:verification_id] = response['request_id']
     redirect '/verify'
   else
@@ -40,17 +62,29 @@ post '/start_verification' do
   end
 end
 
+# Code Verify
+# - shows the user a form to fill in the code
+#   they received via text message
+#
 get '/verify' do
   erb :verify
 end
 
+# Finish Verification
+# - finishes the verification process,
+#   confirming the user submitted code
+#   against the verification ID
+#
 post '/finish_verification' do
+  # check the code with nexmo
   response = nexmo.check_verification(
     session[:verification_id],
     code: params[:code]
   )
 
+  # any status that's not '0' is an error
   if response['status'] == '0'
+    # set the current user to the number
     session[:current_user] = session[:number]
     redirect '/'
   else
@@ -59,6 +93,10 @@ post '/finish_verification' do
   end
 end
 
+# Logout
+# - clears the session and redirects the
+#   user back to the Index page
+#
 get '/logout' do
   session.clear
   redirect '/'
